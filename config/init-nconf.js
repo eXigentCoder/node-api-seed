da0e701a74ed2b57524bc906381ccba3638389a4
Ryan Kotzen <ryan@encentivize.co.za>
2016-10-21 08:31:30 +0200

    updating config

--- config/init-nconf.js
+++ config/init-nconf.js
@@ -1,14 +1,15 @@
 'use strict';
-require('fs'); //must be required here in order to load files from disk.
+var _ = require('lodash');
 var nconf = require('nconf');
+var envDefault = {NODE_ENV: 'development'};
 nconf.argv()
     .env()
-    .defaults({NODE_ENV: 'development'});
+    .defaults(envDefault);
 var environment = nconf.get('NODE_ENV');
 //eslint-disable-next-line no-process-env
 process.env.NODE_ENV = environment;
-//todo RK instead of json file, require a .js file, merge that and pass to overrides/defaults
-var defaultFilePath = './config/default.json';
-var envFilePath = './config/' + environment + '.json';
-nconf.file('envFile', envFilePath);
-nconf.file('defaultFile', defaultFilePath);
+var defaultConfig = require('./default.js');
+var envFilePath = './' + environment + '.js';
+var environmentConfig = require(envFilePath);
+_.merge(envDefault, defaultConfig, environmentConfig);
+nconf.defaults(envDefault);
