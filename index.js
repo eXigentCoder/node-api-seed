17614063b70342fee561145b7078f514a69d5785
Ryan Kotzen <ryan@encentivize.co.za>
2016-10-21 16:16:29 +0200

    adding basic tests.

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ var config = require('nconf');
 var packageJson = require('./package.json');
 var port = config.get('port');
 var createApp = require('./src/app.js');
-//var scheduler = require('./src/jobs/scheduler.js');
 
 createApp(function (err, app) {
     if (err) {
         throw err;
     }
     app.listen(port, function () {
-        //scheduler();
         console.info(packageJson.name + ' is listening on port ' + port);
     });
 });
---
+++ test/init.js
@@ -0,0 +1,11 @@
+'use strict';
+var chai = require('chai');
+var dirtyChai = require('dirty-chai');
+chai.use(dirtyChai);
+require('../config/init-nconf');
+require('../src/logging/index');
+
+global.chai = chai;
+global.expect = chai.expect;
+global.assert = chai.assert;
+global.should = chai.should();
