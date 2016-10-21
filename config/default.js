22633c15d294b8a79c3a1e6003c5152f4d348d9e
Ryan Kotzen <ryan@encentivize.co.za>
2016-10-21 14:02:24 +0200

    Moving logging options to config.

--- config/default.js
+++ config/default.js
@@ -1,19 +1,47 @@
 'use strict';
 module.exports = {
-    "port": 10010,
-    "logging": {
-        "file": {
-            "folder": "./logs",
-            "retention": {
-                "units": "days",
-                "amount": 7
-            }
+    port: 10010,
+    logging: {
+        console: {
+            disabled: false,
+            level: 'silly',
+            colorize: true,
+            timestamp: true,
+            json: true,
+            stringify: false,
+            prettyPrint: true,
+            depth: 10,
+            humanReadableUnhandledException: true,
+            showLevel: true,
+            handleExceptions: true
         },
-        "loggly": {
-            "username": "",
-            "password": "",
-            "token": "",
-            "subdomain": ""
+        file: {
+            disabled: false,
+            level: 'silly',
+            timestamp: true,
+            filename: '.log',
+            maxsize: 1048576, //1m. This is per log file, in bytes, will be split if the log file gets to this size.
+            json: true,
+            prettyPrint: true,
+            depth: 10,
+            showLevel: true,
+            zippedArchive: true,
+            handleExceptions: true,
+            datePattern: './logs/yyyy-MM-ddTHH',
+            prepend: true
+        },
+        loggly: {
+            disabled: true,
+            level: 'silly',
+            json: true,
+            inputToken: '',
+            subdomain: '',
+            auth: {
+                username: '',
+                password: ''
+            },
+            tags: [],
+            handleExceptions: true
         }
     }
 };
--- src/logging/index.js
+++ src/logging/index.js
@@ -5,104 +5,81 @@ require('winston-loggly');
 require('winston-daily-rotate-file');
 var config = require('nconf');
 var formatArgs = require('./format-args');
+var _ = require('lodash');
 var logSettings = config.get('logging');
-var environment = config.get('NODE_ENV');
 var fs = require('fs');
+var path = require('path');
 var logger = new winston.Logger({exitOnError: true});
+addConsoleLogging();
+addFileLogging();
+addLogglyLogging();
+overrideConsole();
 
-switch (environment.toLowerCase()) {
-    case 'production':
-    case 'qa':
-        addFileLogging('silly');
-        addConsoleLogging('silly');
-        addLogglyLogging('silly');
-        break;
-    case 'test':
-        addFileLogging('silly');
-        addConsoleLogging('silly');
-        break;
-    default:
-        addFileLogging('silly');
-        addConsoleLogging('silly');
-        break;
-}
-
-function addConsoleLogging(level) {
-    logger.add(winston.transports.Console, {
-        level: level,
-        colorize: true,
-        timestamp: true,
-        json: false,
-        stringify: false,
-        prettyPrint: true,
-        depth: 10,
-        humanReadableUnhandledException: true,
-        showLevel: true,
-        handleExceptions: true
-    });
-}
-
-function addFileLogging(level) {
-    (function ensureLogFolderExists() {
-        fs.mkdir(logSettings.file.folder, function (error) {
-            if (error && error.code !== 'EEXIST') {
-                throw error;
-            }
-        });
-    }());
-
-    logger.add(winston.transports.DailyRotateFile, {
-        name: 'file',
-        datePattern: '.yyyy-MM-ddTHH',
-        filename: logSettings.file.folder + '/' + environment + '-' + level,
-        level: level,
-        colorize: true,
-        handleExceptions: true
-    });
-}
-
-function addLogglyLogging(level) {
-    var logglySettings = logSettings.loggly;
-    if (!logglySettings.token) {
-        console.warn("Loggly settings not provided, skipping");
+function addConsoleLogging() {
+    var consoleSettings = logSettings.console;
+    if (consoleSettings.disabled) {
         return;
     }
-    logger.add(winston.transports.Loggly, {
-        level: level,
-        json: true,
-        inputToken: logglySettings.token,
-        subdomain: logglySettings.subdomain,
-        auth: {
-            username: logglySettings.username,
-            password: logglySettings.password
-        },
-        tags: [packageJson.name],
-        handleExceptions: true
+    logger.add(winston.transports.Console, _.omit(consoleSettings, 'disabled'));
+}
+
+function addFileLogging() {
+    var fileSettings = logSettings.file;
+    if (fileSettings.disabled) {
+        return;
+    }
+    var folder = path.dirname(fileSettings.datePattern);
+    ensureLogFolderExists(folder);
+    logger.add(winston.transports.DailyRotateFile, _.omit(fileSettings, 'disabled'));
+}
+function ensureLogFolderExists(folder) {
+    if (!folder) {
+        return;
+    }
+    fs.mkdir(folder, function (error) {
+        if (error && error.code !== 'EEXIST') {
+            throw error;
+        }
     });
 }
 
-console.error = function () {
-    logger.error.apply(logger, formatArgs(arguments));//level 0
-};
-console.warn = function () {
-    logger.warn.apply(logger, formatArgs(arguments));//level 1
-};
-console.info = function () {
-    logger.info.apply(logger, formatArgs(arguments));//level 2
-};
-console.log = function () {
-    logger.info.apply(logger, formatArgs(arguments));//level 2
-};
-console.verbose = function () {
-    logger.verbose.apply(logger, formatArgs(arguments));//level 3
-};
-console.debug = function () {
-    logger.debug.apply(logger, formatArgs(arguments));//level 4
-};
-console.silly = function () {
-    logger.silly.apply(logger, formatArgs(arguments));//level 5
-};
+function addLogglyLogging() {
+    var logglySettings = logSettings.loggly;
+    if (logglySettings.disabled) {
+        return;
+    }
+    if (_.isNil(logglySettings.tags)) {
+        logglySettings.tags = [];
+    }
+    if (!_.isArray(logglySettings.tags)) {
+        throw new TypeError("Tags should be an array of strings");
+    }
+    if (logglySettings.tags.indexOf(packageJson.name) < 0) {
+        logglySettings.tags.push(packageJson.name);
+    }
+    logger.add(winston.transports.Loggly, _.omit(logglySettings, 'disabled'));
+}
 
-
-
-
+function overrideConsole() {
+    console.error = function () {
+        logger.error.apply(logger, formatArgs(arguments));//level 0
+    };
+    console.warn = function () {
+        logger.warn.apply(logger, formatArgs(arguments));//level 1
+    };
+    console.info = function () {
+        logger.info.apply(logger, formatArgs(arguments));//level 2
+    };
+    console.log = function () {
+        logger.info.apply(logger, formatArgs(arguments));//level 2
+    };
+    console.verbose = function () {
+        logger.verbose.apply(logger, formatArgs(arguments));//level 3
+    };
+    console.debug = function () {
+        logger.debug.apply(logger, formatArgs(arguments));//level 4
+    };
+    console.silly = function () {
+        logger.silly.apply(logger, formatArgs(arguments));//level 5
+    };
+}
