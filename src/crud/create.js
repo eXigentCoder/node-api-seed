86f80c50c4200e5bceef062794bc773bc81a18f2
Ryan Kotzen <ryan@encentivize.co.za>
2016-10-23 20:28:02 +0200

    moving sendCreateResult to create where it belongs.

--- src/crud/create.js
+++ src/crud/create.js
@@ -21,9 +21,25 @@ function getSteps(router, options) {
         validate: getValidateFunction(schemaName),
         create: options.crudMiddleware.create,
         filterOutput: outputMap.filterOutput,
-        sendCreateResult: options.crudMiddleware.sendCreateResult
+        sendCreateResult: sendCreateResult(router.metadata)
     };
     return applyMaps(options.maps, steps);
+}
+
+function sendCreateResult(metadata) {
+    return function (req, res) {
+        var fullUrl;
+        var id = req.process.output[metadata.identifierName];
+        if (metadata.createdItemLocationHeader) {
+            fullUrl = metadata.createdItemLocationHeader;
+        } else {
+            fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        }
+        fullUrl += '/' + id;
+        return res.status(201)
+            .location(fullUrl)
+            .json(req.process.output);
+    };
 }
 
 function description(metadata) {
