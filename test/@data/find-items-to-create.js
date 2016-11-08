8386cefd6b703a66ff59718e15c37937cdbdefd4
Ryan Kotzen <ryan@encentivize.co.za>
2016-11-08 18:53:02 +0200

    Cleaning up how data is sourced for tests.

--- test/@data/find-items-to-create.js
+++ test/@data/find-items-to-create.js
@@ -1,30 +1,67 @@
 'use strict';
 var glob = require('glob');
 var async = require('async');
+var path = require('path');
+var _ = require('lodash');
 
-module.exports = function findCollections(data, callback) {
-    async.each(data.collections, findDataForCollection, allCollectionsSearched);
-    function allCollectionsSearched(err) {
-        return callback(err, data);
-    }
+module.exports = function (callback) {
+    async.waterfall([
+        findCollections,
+        findDataForAllCollections
+    ], callback);
 };
 
+function findCollections(callback) {
+    var folderPath = path.join(__dirname, '/*/');
+    glob(folderPath, collectionsFound);
+
+    function collectionsFound(collectionFindError, results) {
+        if (collectionFindError) {
+            return callback(collectionFindError);
+        }
+        var data = {
+            collections: []
+        };
+        results.forEach(function (result) {
+            data.collections.push({
+                name: path.basename(result),
+                globPath: result
+            });
+        });
+        return callback(null, data);
+    }
+}
+
+function findDataForAllCollections(data, callback) {
+    async.each(data.collections, findDataForCollection, done);
+
+    function done(err) {
+        return callback(err, data);
+    }
+}
+
 function findDataForCollection(collection, callback) {
-    glob('./tests/data/' + collection.name + '/*.js', function (err, results) {
+    var dataPath = path.join(__dirname, collection.name, '/*.js');
+    glob(dataPath, dataFound);
+
+    function dataFound(err, results) {
         if (err) {
             return callback(err);
         }
         collection.files = [];
         results.forEach(function (result) {
-            var parts = result.split('/');
-            var getDataFunction = require(result.replace('./tests/data/','./'));
+            var requirePath = './' + path.relative(__dirname, result).replace(/\\/g, '/');
+            var fileData = require(requirePath);
+            if (_.isFunction(fileData)) {
+                fileData = fileData();
+            }
             var file = {
-                filename: parts[4],
+                filename: path.basename(result),
                 globPath: result,
-                data: getDataFunction()
+                data: fileData
             };
             collection.files.push(file);
         });
         return callback();
-    });
+    }
 }
