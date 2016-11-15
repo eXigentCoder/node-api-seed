9123de51896d2c1b492da9afb17c9dda12fdee0d
Ryan Kotzen <ryan@encentivize.co.za>
2016-11-15 08:45:57 +0200

    updating passport to use boom.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,23 @@ var authentication = require('./authentication');
 var users = require('./users');
 var rateLimit = require('../rate-limit');
 var passport = require("passport");
+var boom = require('boom');
 module.exports = router;
 
 router.use('/', base);
 router.use('/authentication', authentication);
 router.use(rateLimit.api);
 
-router.use(passport.authenticate('jwt', {session: false}));
+router.use(function authenticate(req, res, next) {
+    passport.authenticate('jwt', {session: false}, authenticationCallback)(req, res);
+    function authenticationCallback(user, authenticated, err) {
+        if (err || !req.user || !authenticated) {
+            return next(boom.unauthorized());
+        }
+        return next();
+    }
+});
+
+
 router.use('/users', users);
 
