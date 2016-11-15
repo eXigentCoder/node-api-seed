025eb7bb12460b72f2083dc55396325a2f6a1ff6
Ryan Kotzen <ryan@encentivize.co.za>
2016-11-15 08:19:06 +0200

    code to generate the query route.

--- test/routes/users/generated.js
+++ test/routes/users/generated.js
@@ -1,14 +1,26 @@
 'use strict';
 var common = require('../../@util/integration-common.js');
-describe('Users', function(){
-    describe('Search for Users', function(){
+describe('Users', function () {
+    this.timeout(common.defaultTimeout);
+    describe('Search for Users', function () {
+        it('Happy case', function (done) {
+            common.request.get('/users')
+                .expect(common.success(200))
+                .set(common.authentication())
+                .expect(common.matchesSwaggerSchema)
+                .end(done);
+        });
     });
-    describe('Get User By Email.', function(){
+    describe('Get User By Email.', function () {
+        //getById
     });
-    describe('Posts Through An User To Be Created.', function(){
+    describe('Posts Through An User To Be Created.', function () {
+        //create
     });
-    describe('Updates a User By Email', function(){
+    describe('Updates a User By Email', function () {
+        //update
     });
-    describe('Updates the status of a User By Email', function(){
+    describe('Updates the status of a User By Email', function () {
+        //update status
     });
 });
--- utilities/generate-test/index.js
+++ utilities/generate-test/index.js
@@ -6,13 +6,15 @@ var eol = require('os').EOL;
 var fileOptions = {encoding: 'utf8'};
 var indentationTemplate = "    ";
 var path = require('path');
+var _ = require('lodash');
 
 (function setInputs() {
     var data = {
         //eslint-disable-next-line global-require
         router: require('../../src/routes/users/index'),
         tagsToGenerateFor: ['User'],
-        outputPath: './test/routes/users/generated.js'
+        outputPath: './test/routes/users/generated.js',
+        routePrefix: '/users'
     };
     findRoutes(data);
 }());
@@ -36,12 +38,13 @@ function writeRoutesAsTest(data) {
     var indent = 0;
     addLine("'use strict';");
     addLine("var common = require('" + path.relative(data.outputPath, './test/@util/integration-common.js').replace(/\\/g, '/').replace('../', '') + "');");
-    addLine("describe('" + data.router.metadata.titlePlural + "', function(){");
+    addLine("describe('" + data.router.metadata.titlePlural + "', function () {");
     indent++;
+    addLine("this.timeout(common.defaultTimeout);");
     data.foundRoutes.forEach(function (foundRoute) {
-        addLine("describe('" + foundRoute.summary + "', function(){");
+        addLine("describe('" + foundRoute.summary + "', function () {");
         indent++;
-
+        addRoute(foundRoute);
         indent--;
         addLine("});");
     });
@@ -49,8 +52,82 @@ function writeRoutesAsTest(data) {
     addLine("});");
     //eslint-disable-next-line no-sync
     fs.writeFileSync(data.outputPath, outputContent, fileOptions);
+
     function addLine(line) {
         outputContent += addLineWithIndent(line, indent);
+    }
+
+    function addRoute(foundRoute) {
+        if (foundRoute.verb === 'get') {
+            if (foundRoute.path.indexOf(':') >= 0) {
+                return addGetById(foundRoute);
+            }
+            return addQuery(foundRoute);
+        }
+        if (foundRoute.verb === 'post') {
+            addCreate(foundRoute);
+        }
+        if (foundRoute.verb === 'put') {
+            var newStausParam = foundRoute.parameters.some((param)=> {
+                return param.name === 'newStatusName';
+            });
+            if (newStausParam) {
+                return addUpdateStatus(foundRoute);
+            }
+            addUpdate(foundRoute);
+        }
+        if (foundRoute.verb === 'delete') {
+            addDelete(foundRoute);
+        }
+    }
+
+    function addQuery(foundRoute) {
+        addLine("it('Happy case', function (done) {");
+        indent++;
+        var url = (data.routePrefix || '') + foundRoute.path;
+        if (_.endsWith(url, '/')) {
+            url = url.substr(0, url.length - 1);
+        }
+        addLine("common.request.get('" + url + "')");
+        indent++;
+        addLine(".expect(common.success(200))");
+        addLine(".set(common.authentication())");
+        addLine(".expect(common.matchesSwaggerSchema)");
+        addLine(".end(done);");
+        indent--;
+        indent--;
+        addLine("});");
+
+        /*
+         it('Get', function (done) {
+         common.request.get('/')
+         .expect(common.success(200))
+         .expect(common.matchesSwaggerSchema)
+         .end(done);
+         });
+         */
+    }
+
+    function addGetById(foundRoute) {
+        addLine('//getById');
+    }
+
+    function addCreate(foundRoute) {
+        addLine('//create');
+    }
+
+    function addUpdate(foundRoute) {
+
+        addLine('//update');
+    }
+
+    function addUpdateStatus(foundRoute) {
+
+        addLine('//update status');
+    }
+
+    function addDelete(foundRoute) {
+        addLine('//delete');
     }
 }
 
