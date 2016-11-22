a68081bbac99f47f522596f4e62b3ab9ae3a09dc
Ryan Kotzen <ryan@encentivize.co.za>
2016-11-22 10:00:08 +0200

    updating code to pull in default user _id.

--- test/@data/items/for-deletion.js
+++ test/@data/items/for-deletion.js
@@ -2,24 +2,29 @@
 var ObjectId = require('mongodb').ObjectId;
 var moment = require('moment');
 var uuid = require("node-uuid");
+var config = require('nconf');
 
-var now = moment.utc().toDate();
-var userId = ObjectId('580d9f45622d510b044fb6a8');
-module.exports = {
-    name: 'item3',
-    description: 'Really cool item, that is about to be deleted',
-    owner: userId,
-    ownerDate: now,
-    ownerLog: [{
+module.exports = function (callback) {
+    var defaultUser = config.get('tests').defaultUser;
+    var now = moment.utc().toDate();
+    var userId = ObjectId(defaultUser._id);
+    var item = {
+        name: 'item3',
+        description: 'Really cool item, that is about to be deleted',
         owner: userId,
-        data: null,
-        ownerDate: now
-    }],
-    versionInfo: {
-        dateCreated: now,
-        versionTag: uuid.v4(),
-        dateUpdated: now,
-        createdBy: userId,
-        lastUpdatedBy: userId
-    }
+        ownerDate: now,
+        ownerLog: [{
+            owner: userId,
+            data: null,
+            ownerDate: now
+        }],
+        versionInfo: {
+            dateCreated: now,
+            versionTag: uuid.v4(),
+            dateUpdated: now,
+            createdBy: userId,
+            lastUpdatedBy: userId
+        }
+    };
+    return callback(null, item);
 };
