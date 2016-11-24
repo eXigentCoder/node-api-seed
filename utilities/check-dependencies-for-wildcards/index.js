143b809e9fd8ec85161f4025d10172225453d225
Ryan Kotzen <ryan@encentivize.co.za>
2016-11-24 12:47:15 +0200

    adding code to ensure no wildcards in dependencies.
    Updating ci steps.

---
+++ utilities/check-dependencies-for-wildcards/index.js
@@ -0,0 +1,18 @@
+'use strict';
+var packageJson = require('../../package.json');
+var util = require('util');
+console.log("Checking dependencies for wildcards");
+var dependencySections = [packageJson.dependencies, packageJson.devDependencies];
+dependencySections.forEach(checkDependencySection);
+
+function checkDependencySection(dependencySection) {
+    Object.keys(dependencySection).forEach(function (dependencyName) {
+        var versionString = dependencySection[dependencyName];
+        var allowedCharacters = /^[a-zA-Z\d\.]{1,}$/;
+        var valid = allowedCharacters.test(versionString);
+        if (!valid) {
+            throw new Error(util.format("Dependency %s has a version string (%s) with invalid characters", dependencyName, versionString));
+        }
+    });
+}
+console.log("All dependencies are wildcard free");
