bd5a4b38eda17474b1310fe60209dd6e36c46567
Ryan Kotzen <ryan@encentivize.co.za>
2016-11-24 13:15:27 +0200

    adding exception for http dependencies.

--- utilities/check-dependencies-for-wildcards/index.js
+++ utilities/check-dependencies-for-wildcards/index.js
@@ -8,6 +8,10 @@ dependencySections.forEach(checkDependencySection);
 function checkDependencySection(dependencySection) {
     Object.keys(dependencySection).forEach(function (dependencyName) {
         var versionString = dependencySection[dependencyName];
+        if (versionString.startsWith('http')) {
+            console.warn(util.format('Http dependency detected (%s), skipping.', versionString));
+            return;
+        }
         var allowedCharacters = /^[a-zA-Z\d\.]{1,}$/;
         var valid = allowedCharacters.test(versionString);
         if (!valid) {
