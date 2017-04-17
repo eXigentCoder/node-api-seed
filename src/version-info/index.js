11307998cfd0c882047fdac4c5055ef15d2e2c89
Ryan Kotzen <ryan@encentivize.co.za>
2017-04-17 16:56:45 +0200

    Updating the version info class to allow updating objects directly, outside of middleware.

--- src/version-info/index.js
+++ src/version-info/index.js
@@ -4,28 +4,38 @@ const moment = require('moment');
 const config = require('nconf');
 
 module.exports = {
-    add: add,
-    update: update
+    add,
+    update,
+    addVersionInfoToObject,
+    updateVersionInfoOnObject
 };
 
 function add(req, res, next) {
     const correlationIdOptions = config.get('logging').correlationId;
-    req.body.versionInfo = {
+    addVersionInfoToObject(req.body, req.user, req.get(correlationIdOptions.reqHeader));
+    return next();
+}
+
+function addVersionInfoToObject(object, user, requestId) {
+    object.versionInfo = {
         dateCreated: moment.utc().toDate(),
         versionTag: uuid.v4(),
         dateUpdated: moment.utc().toDate(),
-        createdBy: req.user._id,
-        lastUpdatedBy: req.user._id,
-        updatedByRequestId: req.get(correlationIdOptions.reqHeader)
+        createdBy: user._id,
+        lastUpdatedBy: user._id,
+        updatedByRequestId: requestId
     };
-    return next();
 }
 
 function update(req, res, next) {
     const correlationIdOptions = config.get('logging').correlationId;
-    req.body.versionInfo.versionTag = uuid.v4();
-    req.body.versionInfo.dateUpdated = moment.utc().toDate();
-    req.body.versionInfo.lastUpdatedBy = req.user._id;
-    req.body.versionInfo.updatedByRequestId = req.get(correlationIdOptions.reqHeader);
+    updateVersionInfoOnObject(req.body, req.user, req.get(correlationIdOptions.reqHeader));
     next();
+}
+
+function updateVersionInfoOnObject(object, user, requestId) {
+    object.versionInfo.versionTag = uuid.v4();
+    object.versionInfo.dateUpdated = moment.utc().toDate();
+    object.versionInfo.lastUpdatedBy = user._id;
+    object.versionInfo.updatedByRequestId = requestId;
 }
